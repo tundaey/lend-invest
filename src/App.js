bc6dd70109cf5da20e38b7da7d5f2e6c128f85ef
CeX <tundaey@gmail.com>
2018-02-11 15:33:17 +0000

    wired up the loans component to the main component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Loans from './components/Loans'
+import {loans} from './current-loans.json'
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      title: 'LendInvest',
+      isModalOpen: false,
+      loans: [],
+      selectedLoan: {},
+      possibleInvestments: []
+    }
+  }
+
+  componentDidMount(){
+    this.setState({loans: loans, possibleInvestments: loans})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8">
+              <br/>
+              <div className="col-md-6"><h1>All Current Loans</h1></div>
+              <div className="col-md-6 Investments-title"><h3>Possible Investments: {this.state.possibleInvestments.length}</h3></div>
+            </div>
+            <div className="col-md-11">
+              <Loans 
+                openModal={this.openModal}
+                isModalOpen={this.state.isModalOpen} 
+                closeModal={this.closeModal} 
+                loans={this.state.loans}/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
